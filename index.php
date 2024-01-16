576db2252a0d398ef7f10775050cdcc9c58960df
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2024-01-16 12:38:33 +0530

    feat: display the data in a tabular format

--- index.php
+++ index.php
@@ -1,30 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Ring 0 Services</title>
+	</head>
 
-$list = [];
-$row  = [];
+	<?php
+		
+	$list = [];
+	$row  = [];
 
-exec("sc query type=kernel", $output);
+	exec("sc query type=kernel", $output);
 
-foreach ($output as $line) {
-    $line = explode(":", $line);
+	foreach ($output as $line) {
+		$line = explode(":", $line);
 
-    if (trim($line[0]) === "SERVICE_NAME") {
-        $row["SERVICE_NAME"] = trim($line[1]);
-    } elseif (trim($line[0]) === "DISPLAY_NAME") {
-        $row["DISPLAY_NAME"] = trim($line[1]);
-    } elseif (trim($line[0]) === "TYPE") {
-        $type                = explode("  ", trim($line[1]));
-        $row["TYPE"]["ID"]   = $type[0];
-        $row["TYPE"]["NAME"] = $type[1];
-    } elseif (trim($line[0]) === "STATE") {
-        $state                = explode("  ", trim($line[1]));
-        $row["STATE"]["ID"]   = $state[0];
-        $row["STATE"]["NAME"] = $state[1];
-        array_push($list, $row);
-        $row = [];
-    } else {
-        continue;
-    }
-}
+		if (trim($line[0]) === "SERVICE_NAME") {
+			$row["SERVICE_NAME"] = trim($line[1]);
+		} elseif (trim($line[0]) === "DISPLAY_NAME") {
+			$row["DISPLAY_NAME"] = trim($line[1]);
+		} elseif (trim($line[0]) === "STATE") {
+			$state                = explode("  ", trim($line[1]));
+			$row["STATE"]["ID"]   = $state[0];
+			$row["STATE"]["NAME"] = $state[1];
+			array_push($list, $row);
+			$row = [];
+		} else {
+			continue;
+		}
+	}
+	
+	?>
 
-var_dump($list);
+	<body>
+		<h1>List of services running in Ring 0</h1>
+
+		<table>
+			<thead>
+				<tr>
+					<td>#</td>
+					<td>Service Name</td>
+					<td>Display Name</td>
+					<td>State</td>
+				</tr>
+			</thead>
+
+			<tbody>
+				<?php for ($i = 0; $i < count($list); $i++): ?>
+				<tr>
+					<td><?php echo $i + 1; ?></td>
+					<td><?php echo $list[$i]["SERVICE_NAME"]; ?></td>
+					<td><?php echo $list[$i]["DISPLAY_NAME"]; ?></td>
+					<td><?php echo $list[$i]["STATE"]["NAME"]; ?></td>
+				</tr>
+				<?php endfor;?>
+			</tbody>
+		</table>
+	</body>
+</html>
